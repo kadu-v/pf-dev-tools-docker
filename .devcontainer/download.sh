f48f6b52a534fe575fb6f1abd9c71c7583024104
kadu-v <kazuonsunday@yahoo.co.jp>
2023-12-17 02:07:09 +0000

    Fix: Dockefile and download.sh

--- .devcontainer/download.sh
+++ .devcontainer/download.sh
@@ -4,7 +4,7 @@ wget \
   --no-verbose --show-progress \
   --progress=bar:force:noscroll \
   https://downloads.intel.com/akdlm/software/acdsinst/23.1std/991/ib_installers/QuartusLiteSetup-23.1std.0.991-linux.run
-# wget \
-#   --no-verbose --show-progress \
-#   --progress=bar:force:noscroll \
-#   https://downloads.intel.com/akdlm/software/acdsinst/23.1std/991/ib_installers/cyclonev-23.1std.0.991.qdz
+wget \
+  --no-verbose --show-progress \
+  --progress=bar:force:noscroll \
+  https://downloads.intel.com/akdlm/software/acdsinst/23.1std/991/ib_installers/cyclonev-23.1std.0.991.qdz
