99fdb9d4e73e208f7b0fe31d2992837731c78712
kadu-v <kazuonsunday@yahoo.co.jp>
2023-12-17 00:40:12 +0000

    Update: for macos

---
+++ .devcontainer/download.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget \
+  --no-verbose --show-progress \
+  --progress=bar:force:noscroll \
+  https://downloads.intel.com/akdlm/software/acdsinst/23.1std/991/ib_installers/QuartusLiteSetup-23.1std.0.991-linux.run
+# wget \
+#   --no-verbose --show-progress \
+#   --progress=bar:force:noscroll \
+#   https://downloads.intel.com/akdlm/software/acdsinst/23.1std/991/ib_installers/cyclonev-23.1std.0.991.qdz
